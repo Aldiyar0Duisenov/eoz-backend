133ef086f5e24c93440b072983495a8476024356
Aldiyar0Duisenov <duisenov0202@gmail.com>
2026-08-16 15:34:53 +0500

    sk-parser

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
-const express = require("express");
-const axios = require("axios");
-const cors = require("cors");
-const keywords = require("./keywords.json");
-const mongoose = require("mongoose");
-require("dotenv").config();
-const { GoogleGenAI } = require("@google/genai");
+import express from "express";
+import axios from "axios";
+import cors from "cors";
+import keywords from "./keywords.json" with { type: "json" };
+import mongoose from "mongoose";
+import "dotenv/config";
+import { GoogleGenAI } from "@google/genai";
+import path from "path";
+import fs from "fs";
+import puppeteer from "puppeteer";
+import { fileURLToPath } from "url";
+import * as cheerio from "cheerio";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+const getRandomMs = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1)) + min;
 
 // Инициализация клиентов
 const ai = new GoogleGenAI({
@@ -17,9 +29,10 @@ async function askGemini(dataset) {
   try {
     const response = await ai.interactions.create({
       model: "gemini-3.6-flash",
-      input: `${dataset} из этого массива объектов создай массив id объектов у которых поле name хоть как-то может быть связано с потенциальной закупкой услуг по сервисчам 1С и связванным с ней услугами, при условии что те кто давали название были заинтересованы скрыть названия. К примеру сопровождение информационной системы считается. Верни отфильтрованный массив id. Верни тексчт который можно будет вставить в JSON.parse() без ошибки`,
+      input: `${dataset} из этого массива объектов создай массив id объектов у которых поле name хоть как-то может быть связано с потенциальной закупкой услуг по сервисчам 1С и связванным с ней услугами, при условии что те кто давали название были заинтересованы скрыть названия. К примеру сопровождение информационной системы считается. Верни отфильтрованный массив id. Верни тексчт который можно будет вставить в JSON.parse() без ошибки. Твой ответ должен начаться с [ и кончится ] `,
     });
 
+    console.log(response.output_text);
     return response.output_text;
   } catch (error) {
     console.error("Ошибка запроса к Gemini:", error);
@@ -66,7 +79,7 @@ app.get("/api/advertisements", async (req, res) => {
   try {
     const results = [];
 
-    for (let i = 0; i < keywords.length; i++) {
+    for (let i = 0; i < 5; i++) {
       const response = await axios.post(
         "https://www.eoz.kz/api/uicommand/get/page",
         {
@@ -204,6 +217,7 @@ app.patch("/api/advertisements/:id/status", async (req, res) => {
 app.get("/api/ai_filter", async (req, res) => {
   try {
     const advertisements = await Advertisement.find({ status: "new" }).lean();
+    console.log(advertisements.length);
     const dataset = advertisements.map((c) => ({
       id: c.id,
       name: c.nameru,
@@ -232,6 +246,139 @@ app.get("/api/ai_filter", async (req, res) => {
     res.status(500).json({
       error: "Ошибка чтения БД",
     });
+  }
+});
+
+app.get("/scrape", async (req, res) => {
+  let browser;
+  try {
+    const downloadPath = path.resolve(__dirname, "downloads");
+    if (!fs.existsSync(downloadPath)) {
+      fs.mkdirSync(downloadPath, { recursive: true });
+    }
+
+    browser = await puppeteer.launch({
+      executablePath:
+        process.env.PUPPETEER_EXECUTABLE_PATH ||
+        "C:/Program Files/Google/Chrome/Application/chrome.exe",
+      args: [
+        "--no-sandbox",
+        "--disable-setuid-sandbox",
+        "--disable-dev-shm-usage",
+        "--disable-gpu",
+        "--disable-blink-features=AutomationControlled",
+        "--start-maximized",
+      ],
+      //headless: false,
+    });
+
+    const page = await browser.newPage();
+
+    // Настраиваем CDP для скачивания на уровне браузера/вкладки
+    const client = await page.target().createCDPSession();
+    await client.send("Page.setDownloadBehavior", {
+      behavior: "allow",
+      downloadPath: downloadPath,
+    });
+
+    await page.setUserAgent(
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
+    );
+
+    await page.evaluateOnNewDocument(() => {
+      Object.defineProperty(navigator, "webdriver", { get: () => false });
+    });
+
+    await page.setViewport({ width: 1680, height: 720 });
+
+    const parsedData = [];
+    async function parseTable(keyword, pageIndex) {
+      const lotStatus = "DISCUSSION_PUBLISHED";
+      await page.goto(
+        `https://zakup.sk.kz/#/ext?tabs=advert&q=${keyword}&adst=${lotStatus}&lst=ALL&page=${pageIndex}`,
+        { waitUntil: "domcontentloaded", timeout: 60000 },
+      );
+
+      console.log("Ждем отрисовки Angular контейнера...");
+      await page.waitForSelector("#infinityScroll", { timeout: 45000 });
+      await delay(getRandomMs(2000, 4000));
+
+      const html = await page.content();
+      const value = cheerio.load(html);
+      const items = value("#infinityScroll .m-found-item").get();
+
+      for (const el of items) {
+        const name = value(".m-found-item__title", el).text().trim();
+        const rawNum = value(".m-found-item__num", el).text().trim();
+        const rawPrice = value(".m-found-item__col--sum .m-span--dark", el)
+          .text()
+          .trim();
+
+        const sum = Number(rawPrice.replace(/\D/g, "")) || 0;
+        const number = Number(rawNum.replace(/\D/g, "")) || 0;
+
+        console.log(`Обрабатываем закупку №${number}...`);
+
+        if (number && !parsedData.find((data) => data.number === number)) {
+          // Передаем существую страницу в функцию скачивания
+
+          parsedData.push({ number, name, sum });
+        }
+
+        //await delay(getRandomMs(2000, 4000));
+      }
+      const xpathSelector = `xpath///ngb-pagination//ul[contains(@class, "pagination")]//a[normalize-space(text())="${pageIndex + 1}"]`;
+
+      // !! превратит (ElementHandle | null) в (true | false)
+      const hasNext = !!(await page.$(xpathSelector));
+      console.log(hasNext);
+      if (hasNext && pageIndex < 4) {
+        await parseTable(keyword, pageIndex + 1); // Рекурсивно вызываем для следующей страницы
+      }
+    }
+
+    for (const keyword of keywords) {
+      await parseTable(keyword, 1);
+    }
+    //await parseTable("сопровождению", 1);
+
+    await Advertisement.bulkWrite(
+      parsedData.map((item) => ({
+        updateOne: {
+          filter: { number: item.number },
+          update: {
+            $set: {
+              id: `${item.number}-${item.number}`,
+              number: item.number,
+              nameru: item.name,
+              namekz: item.name,
+              sum: item.sum,
+              systemNameRu: "СКК",
+              systemNameKz: "СКК",
+              statusNameRu: "Предварительное обсуждение",
+              statusNameKz: "Предварительное обсуждение",
+              dayCount: 7,
+              startDate: `${new Date().toLocaleDateString("ru-RU")}`,
+              endDate: `${new Date(new Date().getTime() + 1000 * 60 * 60 * 24 * 7).toLocaleDateString("ru-Ru")}`,
+              userFavourites: [],
+              customerBin: null,
+              subjectNameRu: null,
+              subjectNameKz: null,
+              externalId: 0,
+            },
+          },
+          upsert: true,
+        },
+      })),
+    );
+
+    res.json({ success: true, count: parsedData.length, result: parsedData });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  } finally {
+    if (browser) {
+      await browser.close();
+    }
   }
 });
 
